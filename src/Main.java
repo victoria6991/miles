ad2583fe2906c5ab055f316a6ae29ac9939469ff
victoria6991 <miss.victoria-liar@yandex.ru>
2021-11-28 12:48:24 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+       int ticketprice = 2000;
+       int mile = 20;
+       int bonusmiles = ticketprice / mile;
+        System.out.println( "Начислено миль за покупку: " + bonusmiles);
+    }
+}
